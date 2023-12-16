977ecf0063a1a58457906828ef84c01d7a770b89
tchebbi <talelkarim.chebbi@insat.ucar.tn>
2023-12-16 21:45:06 +0100

    hide auth values temporarily

---
+++ delete-ns.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+kubectl get namespace "$1" -o json \
+  | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
+  | kubectl replace --raw /api/v1/namespaces/$1/finalize -f -
+
